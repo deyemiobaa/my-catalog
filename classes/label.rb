d3131dc7ed3a804f159e23f195b65e0d44e14ffb
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-04 17:01:22 +0100

    create book and label class

---
+++ classes/label.rb
@@ -0,0 +1,14 @@
+class Label
+  attr_reader :title, :color, :items
+  def initialize(title, color)
+    @id = Random.rand(1..100)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+end
